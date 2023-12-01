353f7e873a7ddb55da7d6306ae9e18329113fc4b
Yuri Karabatov <yuri@yurikarabatov.com>
2023-12-01 17:48:35 +0000

    Extract nums

--- day01-2.go
+++ day01-2.go
@@ -8,31 +8,31 @@ import (
 func day1part2(filename string) (string, error) {
 	var total int
 
+	nums := map[string]int{
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+	}
+
 	if err := forLine(filename, func(line string) {
 		firstIdx := len(line)
 		lastIdx := -1
 		var first, last int
-
-		nums := map[string]int{
-			"1":     1,
-			"2":     2,
-			"3":     3,
-			"4":     4,
-			"5":     5,
-			"6":     6,
-			"7":     7,
-			"8":     8,
-			"9":     9,
-			"one":   1,
-			"two":   2,
-			"three": 3,
-			"four":  4,
-			"five":  5,
-			"six":   6,
-			"seven": 7,
-			"eight": 8,
-			"nine":  9,
-		}
 
 		for k, v := range nums {
 			if f := strings.Index(line, k); f >= 0 && f < firstIdx {
